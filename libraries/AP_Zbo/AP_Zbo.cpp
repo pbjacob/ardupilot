34eb14ed0f855bf0f43ba779dc1d0af40148aa79
pbjacob <pbjacob86@gmail.com>
2017-11-05 16:29:05 +0800

    test libs

---
+++ libraries/AP_Zbo/AP_Zbo.cpp
@@ -0,0 +1,16 @@
+#include "AP_Zbo.h"
+#include <AP_HAL/AP_HAL.h>
+
+
+/// initialise
+bool AP_Zbo::init()
+{
+    return true;
+}
+
+/// deploy
+bool AP_Zbo::deploy()
+{
+    return true;
+}
+
---
+++ libraries/AP_Zbo/AP_Zbo.h
@@ -0,0 +1,13 @@
+/// @file	AP_LandingGear.h
+/// @brief	Landing gear control library
+#pragma once
+
+class AP_Zbo {
+public:
+    /// initialise state of landing gear
+    bool init();
+
+private:
+    /// deploy - deploy the landing gear
+    bool deploy();
+};
